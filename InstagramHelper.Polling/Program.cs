e24c724976dd8bee7bd1a6bd4c1f5586f0434888
Sanccio <sanchesko13@gmail.com>
2026-07-24 15:21:14 +0300

    use user secrets for local configuration

--- InstagramHelper.Polling/Program.cs
+++ InstagramHelper.Polling/Program.cs
@@ -7,19 +7,21 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-IHost host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
-{
-    services.AddLogging(builder => 
-        builder
-            .AddSimpleConsole(c => c.TimestampFormat = "[dd/MM/yy HH:mm:ss] ")
-            .SetMinimumLevel(LogLevel.Debug));
+IHost host = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(config => config.AddUserSecrets<Program>())
+    .ConfigureServices((context, services) =>
+    {
+        services.AddLogging(builder => 
+            builder
+                .AddSimpleConsole(c => c.TimestampFormat = "[dd/MM/yy HH:mm:ss] ")
+                .SetMinimumLevel(LogLevel.Debug));
 
-    services.AddDbContext<InstaHelperDbContext>(opt => 
-        opt.UseSqlServer(context.Configuration.GetConnectionString("InstaHelperConnection")));
+        services.AddDbContext<InstaHelperDbContext>(opt => 
+            opt.UseSqlServer(context.Configuration.GetConnectionString("InstaHelperConnection")));
 
-    services.AddCoreServices(context.Configuration);
-    services.AddHostedService<PollingService>();
-})
-.Build();
+        services.AddCoreServices(context.Configuration);
+        services.AddHostedService<PollingService>();
+    })
+    .Build();
 
 await host.RunAsync();
